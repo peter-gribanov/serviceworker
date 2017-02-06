848a13f6266acb36d5ca35441619b8992a1714c8
Peter Gribanov <wcode404@gmail.com>
2017-02-06 16:56:33 +0300

    handle click on notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,3 +13,23 @@ messaging.setBackgroundMessageHandler(function(payload) {
 
     return self.registration.showNotification(payload.data.title, payload.data);
 });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    console.log(event.notification);
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    // event.waitUntil(clients.matchAll({
+    //     type: "window"
+    // }).then(function(clientList) {
+    //     for (var i = 0; i < clientList.length; i++) {
+    //         var client = clientList[i];
+    //         if (client.url == '/' && 'focus' in client)
+    //             return client.focus();
+    //     }
+    //     if (clients.openWindow)
+    //         return clients.openWindow('/');
+    // }));
+});

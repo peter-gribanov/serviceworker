705ada3f0789fe041558cc24de93dc7bc5d9a9ef
Peter Gribanov <wcode404@gmail.com>
2017-02-06 14:20:08 +0300

    try use firebase.messaging()

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,13 +12,13 @@ firebase.initializeApp({
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-// const messaging = firebase.messaging();
-//
-// // Handle incoming messages. Called when:
-// // - a message is received while the app has focus
-// // - the user clicks on an app notification created by a sevice worker
-// //   `messaging.setBackgroundMessageHandler` handler.
-// messaging.onMessage(function(payload) {
-//     // console.log("Message received. ", payload);
-//     // ...
-// });
+var messaging = firebase.messaging();
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a sevice worker
+//   `messaging.setBackgroundMessageHandler` handler.
+messaging.onMessage(function(payload) {
+    // console.log("Message received. ", payload);
+    // ...
+});

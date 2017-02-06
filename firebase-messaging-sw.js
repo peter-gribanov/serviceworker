b048d925285e4be88f9abafedcc2d2d300a857ab
Peter Gribanov <wcode404@gmail.com>
2017-02-06 17:06:04 +0300

    handle create Notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,29 +13,32 @@ messaging.setBackgroundMessageHandler(function(payload) {
 
     payload.data.data = payload.data;
 
-    var notification = self.registration.showNotification(payload.data.title, payload.data);
-
-    console.log(notification);
-
-    return notification;
+    return self.registration.showNotification(payload.data.title, payload.data).then(function () {
+        console.log(arguments);
+        console.log(this);
+    });
 });
 
 self.addEventListener('notificationclick', function(event) {
     console.log('On notification click: ', event.notification.tag);
     console.log(event.notification);
+    console.log(event.notification.click_action);
     event.notification.close();
 
     // This looks to see if the current is already open and
     // focuses if it is
-    // event.waitUntil(clients.matchAll({
-    //     type: "window"
-    // }).then(function(clientList) {
-    //     for (var i = 0; i < clientList.length; i++) {
-    //         var client = clientList[i];
-    //         if (client.url == '/' && 'focus' in client)
-    //             return client.focus();
-    //     }
-    //     if (clients.openWindow)
-    //         return clients.openWindow('/');
-    // }));
+    event.waitUntil(clients.matchAll({
+        type: 'window'
+    }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == '/' && 'focus' in client) {
+                return client.focus();
+            }
+        }
+
+        if (clients.openWindow) {
+            return clients.openWindow('/');
+        }
+    }));
 });

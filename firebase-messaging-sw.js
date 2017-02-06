d0a1645038881c24d1c98ba837f588963c04dd29
Peter Gribanov <wcode404@gmail.com>
2017-02-06 17:09:48 +0300

    correct handle click on Notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,36 +9,28 @@ const messaging = firebase.messaging();
 
 // Customize notification handler
 messaging.setBackgroundMessageHandler(function(payload) {
-    console.log(payload.data);
-
     payload.data.data = payload.data;
 
-    return self.registration.showNotification(payload.data.title, payload.data).then(function () {
-        console.log(arguments);
-        console.log(this);
-    });
+    return self.registration.showNotification(payload.data.title, payload.data);
 });
 
 self.addEventListener('notificationclick', function(event) {
-    console.log('On notification click: ', event.notification.tag);
-    console.log(event.notification);
-    console.log(event.notification.click_action);
+    var target = event.notification.data.click_action || '/';
     event.notification.close();
 
-    // This looks to see if the current is already open and
-    // focuses if it is
+    // This looks to see if the current is already open and focuses if it is
     event.waitUntil(clients.matchAll({
         type: 'window'
     }).then(function(clientList) {
         for (var i = 0; i < clientList.length; i++) {
             var client = clientList[i];
-            if (client.url == '/' && 'focus' in client) {
+            if (client.url == target && 'focus' in client) {
                 return client.focus();
             }
         }
 
         if (clients.openWindow) {
-            return clients.openWindow('/');
+            return clients.openWindow(target);
         }
     }));
 });

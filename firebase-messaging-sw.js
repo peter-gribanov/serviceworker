70b32bf711e44a94cda875e5166e9f1ecd952b41
Peter Gribanov <wcode404@gmail.com>
2017-02-06 14:56:20 +0300

    remove debug code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,16 +6,3 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-// messaging.setBackgroundMessageHandler(function(payload) {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//
-//     // Customize notification here
-//     const title = 'Background Message Title';
-//     const options = {
-//         body: 'Background Message body.',
-//         icon: '/firebase-logo.png'
-//     };
-//
-//     return self.registration.showNotification(title, options);
-// });

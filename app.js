a91f274a65d6f76b536d336cddc038367b06ad80
Peter Gribanov <wcode404@gmail.com>
2017-02-06 13:18:29 +0300

    use firebase.js in local

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ this.onpush = function(event) {
   console.log(event.data);
   // From here we can write the data to IndexedDB, send it to any open
   // windows, display a notification, etc.
-}
+};
 
 
 var isPushEnabled = false;

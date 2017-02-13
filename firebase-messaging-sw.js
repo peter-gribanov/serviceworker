2c4bc28c4c30f24ce3714279aa6234805d9b7e76
Peter Gribanov <wcode404@gmail.com>
2017-02-13 13:59:45 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,16 +2,17 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '241818759138'
+    'messagingSenderId': '448358493027'
 });
 
 const messaging = firebase.messaging();
 
 // Customize notification handler
 messaging.setBackgroundMessageHandler(function(payload) {
-    payload.data.data = payload.data;
+    console.log(payload);
+    payload.notification.data = payload.data;
 
-    return self.registration.showNotification(payload.data.title, payload.data);
+    return self.registration.showNotification(payload.notification.title, payload.notification);
 });
 
 self.addEventListener('notificationclick', function(event) {
